3f4c5d261fb6af66d0e1a589be312a94176a1a02
Stephen M. Gary <stgary0@gmail.com>
2020-03-10 17:58:16 -0400

    styling

--- user-crad/src/App.js
+++ user-crad/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+import Card from './component/Card'
 import './App.css';
 
 class App extends React.Component {
   state = {
-    users: []
+    users: [],
+    text: ''
   };
 
   componentDidMount() {
@@ -19,10 +21,36 @@ class App extends React.Component {
     .catch(err => console.log(err))
   }
 
+  fetch = e => {
+    e.preventDefault();
+    axios
+    .get(`https://api.github.com/users/${this.state.text}/followers`)
+    .then(res => {
+      console.log(res);
+      this.setState({
+        users: res.data
+      });
+    })
+    .catch(err => console.log(err));
+  }
+
+  handleChanges = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+
   render() {
     return (
       <div>
-
+        <div className='title'>
+          <h1>Users</h1>
+        </div>
+        <div className='search'>
+          <input value={this.state.text} onChange={this.handleChanges} />
+          <button onClick={this.fetch}>Search Users</button>
+        </div>
+        <Card users={this.state.users} />
       </div>
     );
   }
